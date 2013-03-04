1147449eadc3b08ee2c919891f88bdf3841e587d
DCelso <dcelso@gmail.com>
2013-03-04 00:44:36 +0000

    more bugs in build scripts

--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
+svn commit
+svn update
 c:\lazarus\tools\svn2revisioninc.exe .
 c:\lazarus\lazbuild.exe pfpgeditor.lpi
 "c:\Program Files\7-Zip\7z.exe" a -t7z -mx9 fpg-editor.7z fpg-editor.exe lng\*.* zlib\*.*
--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
-svn update
 svn commit
 svn update
 svn2revisioninc .
 lazbuild -B pfpgeditor.lpi
 PVERSION=$(svnversion -n)
-tar -cvzf fpg-editor_${PVERSION}_i386_lin.tgz fpg-editor lng/*
+tar -cvzf fpg-editor_r${PVERSION}_i386lin.tgz fpg-editor lng/*
