1f0e347230356d42da7f922b370a2ac889809246
dcelso@gmail.com <dcelso@gmail.com@844c19e0-f565-0a89-d283-05d7bbb283e7>
2013-03-04 00:36:34 +0000

    no significant changes

--- trunk/build.sh
+++ trunk/build.sh
@@ -1,4 +1,5 @@
 svn commit
 svn2revisioninc .
-lazbuild -B pfpgedit.lpi
-tar -cvzf fpg-editor_$(svn2revisioninc .)_i386_lin.tgz fpg-editor lng/*
+lazbuild -B pfpgeditor.lpi
+PVERSION=$(svnversion -n)
+tar -cvzf fpg-editor_${PVERSION}_i386_lin.tgz fpg-editor lng/*
