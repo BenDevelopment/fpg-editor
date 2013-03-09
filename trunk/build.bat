113cbe099c1ba4098c85c02b0baa46122fac8b3a
dcelso@gmail.com <dcelso@gmail.com@844c19e0-f565-0a89-d283-05d7bbb283e7>
2013-03-09 21:48:04 +0000

    bug fixed in last build.bat uploaded.

--- trunk/build.bat
+++ trunk/build.bat
@@ -2,4 +2,4 @@ svn commit
 svn update
 c:\lazarus\tools\svn2revisioninc.exe .
 c:\lazarus\lazbuild.exe -B pfpgeditor.lpi
-"c:\Program Files\7-Zip\7z.exe" a -t7z -mx9 fpg-editor_r%PVERSION%_i386lin.7z fpg-editor.exe lng\*.* zlib\*.*
+"c:\Program Files\7-Zip\7z.exe" a -t7z -mx9 fpg-editor_r%PVERSION%_win32.7z fpg-editor.exe lng\*.* languages\*.* zlib\*.*
