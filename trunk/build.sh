4a9ffc376c738dc701893926cc20365351630228
dcelso@gmail.com <dcelso@gmail.com@844c19e0-f565-0a89-d283-05d7bbb283e7>
2013-03-21 22:27:47 +0000

    Changes in build.sh to upload to googlecode the final .tgz file

--- trunk/build.sh
+++ trunk/build.sh
@@ -6,5 +6,6 @@ PVERSION=$(svnversion -n)
 #tar -cvzf fpg-editor_r${PVERSION}_i386lin.tgz fpg-editor languages/*.po
 
 wine $HOME/.wine/drive_c/lazarus/lazbuild.exe -B --bm=DefaultWin32 pfpgeditor.lpi
-tar -cvzf fpg-editor_r${PVERSION}_linux_and_win32.tgz fpg-editor fpg-editor.exe languages/*.po
+tar -cvzf fpg-editor_r${PVERSION}_linux_and_win32.tgz fpg-editor fpg-editor.exe zlib/zlib.exe languages/*.po
 
+python googlecode_upload.py -s "r${PVERSION} linx and windows binaries" -p "fpg-editor" fpg-editor_r${PVERSION}_linux_and_win32.tgz
