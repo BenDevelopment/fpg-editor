e88ff1afea7b6d2c4f0b0aa1e2f78390600a17ef
DCelso <dcelso@gmail.com>
2013-03-21 22:17:11 +0000

    Changes in build script to cross compile using wine

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 svn commit
 svn update
 svn2revisioninc .
-lazbuild -B pfpgeditor.lpi
+lazbuild -B --bm=DefaultQT pfpgeditor.lpi
 PVERSION=$(svnversion -n)
-tar -cvzf fpg-editor_r${PVERSION}_i386lin.tgz fpg-editor languages/*
+#tar -cvzf fpg-editor_r${PVERSION}_i386lin.tgz fpg-editor languages/*.po
+
+wine $HOME/.wine/drive_c/lazarus/lazbuild.exe -B --bm=DefaultWin32 pfpgeditor.lpi
+tar -cvzf fpg-editor_r${PVERSION}_linux_and_win32.tgz fpg-editor fpg-editor.exe languages/*.po
+
